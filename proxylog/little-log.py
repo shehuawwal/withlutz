ef8059be2f299dd6d19cf4346b7b1bbb125cf645
Shehu Awwal <awwalshehu@gmail.com>
2022-09-09 11:09:42 +0100

    add proxy log

--- proxylog/little-log.py
+++ proxylog/little-log.py
@@ -1,17 +1,12 @@
 import re
 import json
 
-pattern_time = re.compile(r'\d\d[:]\d\d[:]\d\d')
-pattern_date = re.compile(r'\d\d\d\d[-]\d\d[-]\d\d')
+pattern_time = re.compile(r'\d{2}[:]\d{2}[:]\d{2}')
+pattern_date = re.compile(r'\d{4}[-]\d{2}[-]\d{2}')
 
-with open('little.log', 'r') as f:
-    logs = f.read()
-    
-    pattern_times = pattern_time.findall(logs)
-    pattern_days = pattern_date.findall(logs)
-    ls = zip(pattern_days, pattern_times)
 
-    for patternt in zip(pattern_days, pattern_times):
-            days = patternt[0]
-            time = patternt[1]
-            print(days, time)
+with open("little.log") as f:
+    for line in f:
+        pattern_times = pattern_time.findall(line)
+        pattern_days = pattern_date.findall(line)
+        print(pattern_days[0], pattern_times[1])
